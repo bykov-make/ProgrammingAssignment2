76fec886632723072776d754dd16a51b42d15394
Macarena Ch Bykov <bykov_make@hotmail.com>
2023-03-07 21:00:34 -0300

    Adding comments to the code

--- cachematrix.R
+++ cachematrix.R
@@ -5,14 +5,19 @@
 
 makeCacheMatrix <- function(x = matrix()) {
  m <- NULL
+ #set the value of the matrix
  set <- function(y) {
    x <<- y
    m <<- NULL
  }
  
+ #get the value of the matrix
  get <- function() x
  
+ #set the value of the inverted matrix
  seti <- function(inv) m <<- inv
+ 
+ #get the value of the inverted matrix
  geti <- function() m
  
  list( set = set, get = get,
@@ -26,12 +31,15 @@ cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   m <- x$geti()
   if(!is.null(m)){
+    #return the inverted matrix from cache
      message("Cache Inversed Matrix")
      return(m)
   }
-  
+  #get the values of the matrix
   data <- x$get()
+  #invert the matrix
   m <- solve(data)
+  #set the matrix to m
   x$seti(m)
   m
 }
